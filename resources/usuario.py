0b1cacbe635bdb2acb5b5d75c691ba1731196f61
Nielssouza <souzadaniel187@gmail.com>
2023-01-17 01:00:34 -0300

    Update de versão do flesk

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,6 +1,12 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
+from flask_jwt_extended import create_access_token
+from werkzeug.security import safe_join
 
+
+atributos = reqparse.RequestParser()
+atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
+atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
 
 class User(Resource):
     #/usuario/{user_id}   
@@ -21,14 +27,24 @@ class User(Resource):
 class UserRegister(Resource):
      # /cadastro
     def post(self):
-        atributos = reqparse.RequestParser()
-        atributos.add_argument('login', type=str, required=True, help="The field 'login' cannot be left blank")
-        atributos.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be left blank")
         dados = atributos.parse_args()
-
+        
         if UserModel.find_by_login(dados['login']):
            return {"message": "The login '{}' already exists.".format(dados['login'])}
 
         user = UserModel(**dados)
         user.save_user()
         return{'message': 'User cread sucessfully!'}, 201 #Created
+
+class UserLogin(Resource):
+    
+    @classmethod
+    def post(cls):
+        dados = atributos.parse_args()
+
+        user = UserModel.find_by_login(dados['login'])
+        
+        if user and safe_join(user.senha, dados['senha']):
+           token_de_acesso = create_access_token(identity=user.user_id)
+           return {'acess_token': token_de_acesso}, 200
+        return {'message': 'The username or password is incorrect.'}, 401 #Unauthorized
